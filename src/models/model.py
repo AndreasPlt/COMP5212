fa7b097df6f9452150700905342b29909839a622
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2023-11-12 16:13:08 +0800

    FLATTEN

--- src/models/model.py
+++ src/models/model.py
@@ -7,18 +7,21 @@ def get_model(model_name, num_classes=35, pretrained=True, freeze=True):
     if model_name == 'mobilenet_v2':
         model = mobilenet_v2(pretrained=pretrained)
         model.classifier = torch.nn.Sequential(
+            torch.nn.Flatten(1, -1),
             torch.nn.Dropout(p=0.2, inplace=False),
             torch.nn.Linear(in_features=1280, out_features=num_classes, bias=True)
         )
     elif model_name == 'mobilenet_v3_small':
         model = mobilenet_v3_small(pretrained=pretrained)
         model.classifier = torch.nn.Sequential(
+            torch.nn.Flatten(1, -1),
             torch.nn.Dropout(p=0.2, inplace=False),
             torch.nn.Linear(in_features=1024, out_features=num_classes, bias=True)
         )
     elif model_name == 'mobilenet_v3_large':
         model = mobilenet_v3_large(pretrained=pretrained)
         model.classifier = torch.nn.Sequential(
+            torch.nn.Flatten(1, -1),
             torch.nn.Dropout(p=0.2, inplace=False),
             torch.nn.Linear(in_features=1280, out_features=num_classes, bias=True)
         )
