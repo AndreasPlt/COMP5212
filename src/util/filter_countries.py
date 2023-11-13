3b3dfa632f412a4ac9e16f03690c78608c2a5c8a
AndreasPlt <107055616+AndreasPlt@users.noreply.github.com>
2023-11-13 20:37:45 +0800

    Add function to read valid countries from file

--- src/util/filter_countries.py
+++ src/util/filter_countries.py
@@ -22,6 +22,13 @@ def write_valid_countries(valid_countries, write_file_name):
         for country in valid_countries:
             write_file.write(country + "\n")
 
+def read_valid_countries(read_file_name):
+    countries = []
+    with open(read_file_name, "r") as read_file:
+        for line in read_file:
+            countries.append(line.strip())
+    return countries
+
 def main():
     valid_countries = filter_countries(MIN_IMAGES, DATA_DIR)
     write_valid_countries(valid_countries, os.path.join(DATA_DIR, "valid_countries.txt"))
